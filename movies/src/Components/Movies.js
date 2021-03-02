2d8c9d0d9e3800a28ce5611487281b8b9d8ff38a
StephyMunoz <58042439+StephyMunoz@users.noreply.github.com>
2021-03-01 22:32:38 -0500

    faltan comentarios dinamicos

--- movies/src/Components/Movies.js
+++ movies/src/Components/Movies.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Descriptions, Modal, Col, Row, Button, Card, Comment, Avatar, Form, List, Input } from 'antd';
+import {Descriptions, Tooltip, Modal, Col, Row, Button, Card, Comment, Avatar, Form, List, Input } from 'antd';
 import moment from 'moment';
 
 const { TextArea } = Input;
@@ -52,9 +52,42 @@ const Movies = ({ movies }) => {
             </Form.Item>
         </>
     );
-    const handleComment = () => {
-
-    }
+    const data = [
+        {
+            actions: [<span key="comment-list-reply-to-0">Reply to</span>],
+            author: 'Han Solo',
+            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+            content: (
+                <p>
+                    We supply a series of design principles, practical patterns and high quality design
+                    resources (Sketch and Axure), to help people create their product prototypes beautifully and
+                    efficiently.
+                </p>
+            ),
+            datetime: (
+                <Tooltip title={moment().subtract(1, 'days').format('YYYY-MM-DD HH:mm:ss')}>
+                    <span>{moment().subtract(1, 'days').fromNow()}</span>
+                </Tooltip>
+            ),
+        },
+        {
+            actions: [<span key="comment-list-reply-to-0">Reply to</span>],
+            author: 'Han Solo',
+            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+            content: (
+                <p>
+                    We supply a series of design principles, practical patterns and high quality design
+                    resources (Sketch and Axure), to help people create their product prototypes beautifully and
+                    efficiently.
+                </p>
+            ),
+            datetime: (
+                <Tooltip title={moment().subtract(2, 'days').format('YYYY-MM-DD HH:mm:ss')}>
+                    <span>{moment().subtract(2, 'days').fromNow()}</span>
+                </Tooltip>
+            ),
+        },
+    ];
 
     return(
         <>
@@ -81,12 +114,28 @@ const Movies = ({ movies }) => {
                     <Button type="primary" onClick={()=>handleView(movie.imdbID)} align="middle" style={{ margin: 10 }}>
 
                         Show more information
-                    </Button>
-                    <Button type="primary" onClick={()=>handleComment(movie.imdbID)} align="middle" style={{ margin: 10 }}>
-
-                        Add comment
 
                     </Button>
+
+                        <List
+                            className="comment-list"
+                            header={`${data.length} replies`}
+                            itemLayout="horizontal"
+                            dataSource={data}
+                            renderItem={item => (
+                                <li>
+                                    <Comment
+                                        actions={item.actions}
+                                        author={item.author}
+                                        avatar={item.avatar}
+                                        content={item.content}
+                                        datetime={item.datetime}
+                                    />
+                                </li>
+                            )}
+                        />
+
+
 
                     <Modal title="Movie information" visible={isModalVisible} onCancel={handleCancel} >
                         <Descriptions title="Movie Info" bordered>
